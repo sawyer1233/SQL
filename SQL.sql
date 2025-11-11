a16c6b435d45037a43370a2b2752cc2f879eeb8b
sawyer <jakub.pawlik435@gmail.com>
2025-11-11 16:59:52 +0100

    gg

--- SQL.sql
+++
@@ -1,8 +0,0 @@
-use  hospital
-
-SELECT 
-    YEAR(registration_date) AS registration_year,
-    COUNT(*) AS total_patients
-FROM patients
-GROUP BY YEAR(registration_date)
-ORDER BY registration_year;
